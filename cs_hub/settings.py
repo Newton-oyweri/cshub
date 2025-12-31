aaee934931c7bd69c58366b3d4ef70cc4d5f94a6
Newton-oyweri <oywerinewton@gmail.com>
2025-12-31 22:17:27 +0300

    added icons 2

--- cs_hub/settings.py
+++ cs_hub/settings.py
@@ -137,16 +137,11 @@ PWA_APP_ICONS = [
         'src': '/static/images/icons/logo.png',
         'sizes': '192x192',
         'type': 'image/png'
-    },
-    {
-        'src': '/static/images/icons/logo.png',
-        'sizes': '512x512',
-        'type': 'image/png'
     }
 ]
 PWA_APP_SPLASH_SCREEN = [
     {
-        'src': '/static/images/icons/logo.png',
+        'src': '/static/images/icons/icon-512x512.png',
         'media': '(device-width: 1125px) and (device-height: 2436px) and (-webkit-device-pixel-ratio: 3)'
     }
 ]
